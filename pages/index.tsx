448587e13d0763c1784e9f9e0fa156264b4059ba
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-23 15:56:08 -0300

    Configuração do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+import style from '../styles/HomeNoAuth.module.scss'
+
+export const homeNoAuth = function () {
+  return<>
+    <Head>
+      <title>Onebitflix</title>
+    </Head>
+
+    <main></main>
+  </>
+}
+
+export default homeNoAuth
