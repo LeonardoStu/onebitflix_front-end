6f56830535053713ec518c7e66c54946aba39e19
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-23 17:41:27 -0300

    Criando seção de slides

---
+++ src/components/common/slideCard/index.tsx
@@ -0,0 +1,11 @@
+import style from './style.module.scss'
+
+const SlideCard = function () {
+    return <>
+        <div>
+            <img src="" alt="" className={style.slideImg}/>
+            <p className={style.SlideTitle}></p>
+            <p className={style.SlideDescription}></p>
+        </div>
+    </>
+}
