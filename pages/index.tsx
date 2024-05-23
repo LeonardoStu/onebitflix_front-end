e4d929fb8d848c9f3f7f81c60c2ab85a13552051
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-23 18:43:44 -0300

    Instalação do AOS

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,10 @@ import CardsSection from "@/components/homeNoAuth/cardSection"
 import SlideSection from "@/components/homeNoAuth/slideSection"
 import { GetStaticProps } from "next"
 import courseServices, { CourseType } from "@/services/courseService"
-import { ReactNode } from "react"
+import { ReactNode, useEffect } from "react"
 import Footer from "@/components/common/footer"
+import 'aos/dist/aos.css'
+import AOS from 'aos'
 
 interface IndexPageProps {
   chrildren?: ReactNode
@@ -15,6 +17,11 @@ interface IndexPageProps {
 }
 
 export const homeNoAuth = function ({ course }: IndexPageProps) {
+
+  useEffect(()=> {
+    AOS.init()
+  }, [])
+
   return<>
     <Head>
       <title>Onebitflix</title>
@@ -24,13 +31,17 @@ export const homeNoAuth = function ({ course }: IndexPageProps) {
     </Head>
 
     <main>
-      <div className={style.sectionBackground}>
-        <HeaderNoAuth/>
-        <PresententionSection/>
-      </div>
-      <CardsSection/>
-      <SlideSection newestCourses={course}/> 
-      <Footer/>
+      <div className={style.sectionBackground} data-aos='fade-zoom-in' data-aos-duration='1600'>
+          <HeaderNoAuth/> 
+          <PresententionSection/>
+        </div>
+        <div data-aos='fade-right' data-aos-duration='1200'>
+          <CardsSection />
+        </div>
+        <div data-aos='fade-up' data-aos-duration='1350'>
+          <SlideSection newestCourses={course}/>
+        </div> 
+        <Footer/>
     </main>
   </>
 }
