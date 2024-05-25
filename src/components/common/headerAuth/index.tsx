064aa70be6a6082a166d1b5dcac82f3861a012dc
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-25 11:13:59 -0300

    Criando a estrutura da página homeAuth - Parte 2

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -1,9 +1,30 @@
 import { Container, Form, Input } from 'reactstrap'
 import style from './style.module.scss'
 import Link from 'next/link'
+import Modal from 'react-modal'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
+Modal.setAppElement('#__next')
 
 const HeaderAuth = function () {
+    const router = useRouter()
+    const [OpenModal, setOpenModal] = useState(false)
+
+    const handleOpenModal = function () {
+        setOpenModal(true)
+    }
+
+    const handleCloseModal = function () {
+        setOpenModal(false)
+    }
+
+    const handleLogout = function () {
+        sessionStorage.clear()
+
+        router.push('/')
+    }
+
     return <>
         <Container className={style.nav}>
             <Link href='/home'>
@@ -14,8 +35,14 @@ const HeaderAuth = function () {
                     <Input name='search' type='search' placeholder='Pesquisar' className={style.input}/>
                 </Form>
                 <img src="/homeAuth/iconSearch.svg" alt="Lupa" className={style.searchImg}/>
-                <p className={style.userProfile}>AB</p>
+                <p className={style.userProfile} onClick={handleOpenModal}>AB</p>
             </div>
+            <Modal isOpen={OpenModal} onRequestClose={handleCloseModal} shouldCloseOnEsc={true} className={style.modal} overlayClassName={style.overlayModal}>
+                <Link href='/profile'>
+                    <p className={style.modalLink}>Meus dados</p>
+                </Link>
+                <p className={style.modalLink} onClick={handleLogout}>Sair</p>
+            </Modal>
         </Container>
     </>
 }
