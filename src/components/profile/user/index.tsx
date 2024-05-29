b8252f5d49d7afd398a80607fcffc24e03b422f4
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-05-29 12:57:58 -0300

    Conexão com o backend usuário - parte 1

--- src/components/profile/user/index.tsx
+++ src/components/profile/user/index.tsx
@@ -1,12 +1,61 @@
 import { Button, Form, FormGroup, Input, Label } from "reactstrap"
 import style from "../../../../styles/profile.module.scss"
+import { FormEvent, useEffect, useState } from "react"
+import profileServices from "@/services/profileService"
+import ToasComponent from "@/components/common/toast"
 
 const UserForm = function () {
+    const [color, setColor] = useState("");
+    const [toastIsOpen, setToastIsOpen] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [ firstName, setFirstName] = useState('')
+    const [ lastName, setlastName] = useState('')
+    const [ phone, setPhone] = useState('')
+    const [ email, setEmail] = useState('')
+    const [ created_at, setCreated_at] = useState('')
+
+    useEffect(() => {
+        profileServices.fetchCurrent().then((user) => {
+            setFirstName(user.firstName)
+            setlastName(user.lastName)
+            setPhone(user.phone)
+            setEmail(user.email)
+            setCreated_at(user.created_at)
+        })
+    }, [])
+
+    const handleUserUpdate = async function (event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    
+        const res = await profileServices.userUpdate({
+            firstName,
+            lastName,
+            phone,
+            email,
+            created_at
+        });
+
+        if (res === 200) {
+            setToastIsOpen(true);
+            setErrorMessage("Informações alteradas com sucesso!");
+            setColor("bg-success");
+            setTimeout(() => setToastIsOpen(false), 1000 * 3);
+        } else {
+            setToastIsOpen(true);
+            setErrorMessage("Você não pode mudar para esse email!");
+            setColor("bg-danger");
+            setTimeout(() => setToastIsOpen(false), 1000 * 3);
+        }
+    }
+
     return<>
-        <Form className={style.form}>
+        <Form onSubmit={handleUserUpdate} className={style.form}>
             <div className={style.formName}>
-                <p className={style.nameAbbreviation}>NT</p>
-                <p className={style.userName}>Name Test</p>
+                <p className={style.nameAbbreviation}>
+                    {firstName.slice(0, 1)}
+                    {lastName.slice(0, 1)}
+                </p>
+                <p className={style.userName}>{`${firstName} ${lastName}`}</p>
             </div>
             <div className={style.memberTime}>
                 <img className={style.memberTimeImg} src="/profile/iconUserAccount.svg" alt="iconProfile" />
@@ -18,27 +67,28 @@ const UserForm = function () {
             <div className={style.inputFlexDiv}>
                 <FormGroup>
                     <Label className={style.label} for="firstName">NOME</Label>
-                    <Input name="firstName" type="text" id="firstName" placeholder="Qual o seu primeiro nome?" required maxLength={20} className={style.inputFlex} value={"Name"} />
+                    <Input name="firstName" type="text" id="firstName" placeholder="Qual o seu primeiro nome?" required maxLength={20} className={style.inputFlex} value={firstName} onChange={(ev) => setFirstName(ev.target.value)}/>
                 </FormGroup>
 
                 <FormGroup>
                     <Label className={style.label} for="lastName">SOBRENOME</Label>
-                    <Input name="lastName" type="text" id="lastName" placeholder="Qual o seu último nome?" required maxLength={20} className={style.inputFlex} value={"Test"} />
+                    <Input name="lastName" type="text" id="lastName" placeholder="Qual o seu último nome?" required maxLength={20} className={style.inputFlex} value={lastName} onChange={(ev) => setlastName(ev.target.value)}/>
                 </FormGroup>
             </div>
             <div className={style.inputNormalDiv}>
                 <FormGroup>
                     <Label className={style.label} for="phone">WHATSAPP / TELEGRAM</Label>
-                    <Input name="phone" type="tel" id="phone" placeholder="(xx) xxxxx-xxxx" required maxLength={20} className={style.input} value={"Name"} />
+                    <Input name="phone" type="tel" id="phone" placeholder="(xx) xxxxx-xxxx" required maxLength={20} className={style.input} value={phone} onChange={(ev) => setPhone(ev.target.value)}/>
                 </FormGroup>
 
                 <FormGroup>
                     <Label className={style.label} for="email">EMAIL</Label>
-                    <Input name="email" type="text" id="email" placeholder="Coloque seu email" required className={style.input} value={"test@email.com"} />
+                    <Input name="email" type="text" id="email" placeholder="Coloque seu email" required className={style.input} value={email} onChange={(ev) => setEmail(ev.target.value)}/>
                 </FormGroup>
                 <Button className={style.formBtn}>Salvar alterações</Button>
             </div>
         </Form>
+        <ToasComponent color={color} isOpen={toastIsOpen} message={errorMessage} />
     </>
 }
 
---
+++ src/services/profileService.ts
@@ -0,0 +1,41 @@
+import api from './api'
+
+interface UserParams {
+    firstName: string
+    lastName: string
+    phone: string
+    email: string
+    created_at: string
+}
+
+const profileServices = {
+    fetchCurrent: async () => {
+        const token = sessionStorage.getItem('onebitflix-token')
+        const res = await api.get('/users/current', {
+            headers: {Authorization: `Bearer ${token}`}
+        }).catch((err) => {return err.response})
+
+        return res.data
+    },
+    userUpdate: async (params: UserParams) => {
+        const token = sessionStorage.getItem("onebitflix-token");
+    
+      const res = await api
+      .put("/users/current", params, {
+      headers: {
+          Authorization: `Bearer ${token}`,
+      },
+      })
+      .catch((error) => {
+      if (error.response.status === 400 || error.response.status === 401) {
+          return error.response;
+      }
+    
+        return error;
+      });
+    
+      return res.status;
+    },
+}
+
+export default profileServices
