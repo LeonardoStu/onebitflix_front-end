96ac6e9b419aea2dc66bc1e6ae8d5b9931e55578
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-06-04 11:31:33 -0300

    Criação de estrutura da página de episódios

--- src/components/episodeList/index.tsx
+++ src/components/episodeList/index.tsx
@@ -1,37 +1,44 @@
 import styles from "./style.module.scss";
-import { EpisodeType } from "../../services/courseService";
+import { CourseType, EpisodeType } from "../../services/courseService";
+import { useRouter } from "next/router";
 
 interface props {
   episode: EpisodeType;
+  course: CourseType
 }
 
-const EpisodeList = function ({ episode }: props) {
+const EpisodeList = function ({ episode, course }: props) {
+  const router = useRouter()
 
-    const handleSecondsToMin = (totalSeconds: number) => {
-        const minutes = Math.floor(totalSeconds / 60);
-    
-        const seconds = totalSeconds % 60;
-    
-        function toString(num: number) {
-          return num.toString().padStart(2, "0");
-        }
-    
-        const result = `${toString(minutes)}:${toString(seconds)}`;
-    
-        return result;
-    }
+  const handleSecondsToMin = (totalSeconds: number) => {
+      const minutes = Math.floor(totalSeconds / 60);
+  
+      const seconds = totalSeconds % 60;
+  
+      function toString(num: number) {
+        return num.toString().padStart(2, "0");
+      }
+  
+      const result = `${toString(minutes)}:${toString(seconds)}`;
+  
+      return result;
+  }
+
+  const handleEpisodePlayes = () => {
+    router.push(`/courses/episode/${episode.order - 1}?courseid=${course.id}`)
+  }
 
   return <>
-    <div className={styles.episodeCard}>
-	  <div className={styles.episodeOrderTime}>
-	    <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
-      <p className={styles.episodeTime}>{handleSecondsToMin(episode.secondsLong)}</p>
+    <div className={styles.episodeCard} onClick={handleEpisodePlayes}>
+      <div className={styles.episodeOrderTime}>
+        <p className={styles.episodeOrder}>Episódio Nº {episode.order}</p>
+        <p className={styles.episodeTime}>{handleSecondsToMin(episode.secondsLong)}</p>
+      </div>
+      <div className={styles.episodeTitleDescription}>
+        <p className={styles.episodeTitle}>{episode.name}</p>
+        <p className={styles.episodeDescription}>{episode.synopsis}</p>
+      </div>
     </div>
-    <div className={styles.episodeTitleDescription}>
-	    <p className={styles.episodeTitle}>{episode.name}</p>
-      <p className={styles.episodeDescription}>{episode.synopsis}</p>
-    </div>
-  </div>
   </>;
 };
 
